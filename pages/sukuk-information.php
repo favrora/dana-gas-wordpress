5f86f995b785b6f139b13a06f89c67d772b6f4fe
favrora <david.voskanyan1213@gmail.com>
2023-05-24 23:58:02 +0400

    fix

---
+++ pages/sukuk-information.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Template Name: Sukuk Information
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta info -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php bloginfo("name"); ?></title>
+
+    <!-- Source files -->
+    <link href="<?php echo get_template_directory_uri() . "/dist/css/operations-kri.min.css"; ?>" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+
+<?php get_header(); ?>
+
+<!-- About section -->
+<section class="about-section" style="background-image: url('<?php echo get_template_directory_uri() .
+    "/assets/images/investors-info-bg.png"; ?>')">
+    <div class="about-overlay"></div>
+    <div class="container">
+        <h1>Investors</h1>
+    </div>
+</section>
+
+<!-- Annual reports section -->
+<section class="company-section reports-section top-bg-section pt-5" style="background-image: url('<?php echo get_template_directory_uri() .
+    "/assets/images/about-shape-bg.png"; ?>)">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 text-center mb-2">
+                <h2 class="default-h2">Sukuk information</h2>
+            </div>
+
+            <div class="col-12">
+                <div class="reports-nav">
+                    <div class="reports-nav__item is-active">2018</div>
+                    <div class="reports-nav__item">2017</div>
+                </div>
+
+                <div class="reports-links">
+                    <div class="reports-links__item is-active">
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Buyback-of-12m_16-Dec-2018.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Buyback of $12m <span class="pres-date"> 16 Dec 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Buyback-of-99m_15-Oct-2018.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Buyback of $99m <span class="pres-date"> 15 Oct 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Successfully-completes-Sukuk-Refinance_14-Aug-2018.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Successful completion of Sukuk refinancing <span class="pres-date"> 14 Aug 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Shareholders-support-US700m-Sukuk_21-June-2018.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Shareholders approve US$700m Sukuk <span class="pres-date"> 21 June 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Disclosure-on-the-English-Court-Judgement-and-Orders-of-Fabruary-2018_En.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Disclosure on the English court judgement <span class="pres-date"> 19 Feb 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Sukuk-B.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Sukuk B <span class="pres-date"> 01 Feb 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Sukuk-A.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Sukuk A <span class="pres-date"> 01 Feb 2018 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+                    </div>
+
+                    <div class="reports-links__item">
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Decision-Sharjah_2-Dec-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Decision Sharjah<span class="pres-date"> 02 Dec 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Disclosure-on-UK-KK_19-Nov-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Disclosure on UK KK<span class="pres-date"> 19 Nov 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/OFFERING-CIRCULAR_31-Oct-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Offering circular<span class="pres-date"> 31 Oct 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Disclosure-on-UK-court-appeals_12-Oct-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Disclosure on UK courts appeal<span class="pres-date"> 12 Oct 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/DG-release-CMC-outcomes_1-Aug-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">CMC outcomes<span class="pres-date"> 01 Aug 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Disclosure-re-Exchange-offer_31-July-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Disclosure re-exchange offer<span class="pres-date"> 31 July 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Disclosure-regarding-profit-payment_27-July-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Disclosure regarding profit payment<span class="pres-date"> 27 July 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Compare-Sharjah-injunction-disclosure_24-July-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Compare Sharjah injunction disclosure<span class="pres-date"> 24 July 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+
+                      <a href="https://www.danagas.com/wp-content/uploads/2019/06/Company-Statement-To-Sukuk-Bondholders_6-July-2017.pdf" target="_blank">
+                        <div class="d-flex flex-wrap">Company statement to Sukuk bondholders<span class="pres-date"> 06 July 2017 </span></div>
+                        <img class="arrow" src="<?php echo get_template_directory_uri() .  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+                      </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Cards section -->
+<section class="cards-section story mb-5">
+  <div class="container">
+    <h2 class="default-h2"><?php echo get_field("our_story_title"); ?></h2>
+    <div class="row mt-3">
+
+      <div class="col-12 col-md-4 mb-3">
+        <a href="/investors/reports/" class="card-link">
+          <div class="card">
+            <div class="card-header">
+              <img src="<?php echo get_template_directory_uri() .
+                  "/assets/images/investors/annual-report.png"; ?>" alt="View our Annual report 2019" loading="lazy">
+            </div>
+            <div class="card-body">
+              <div class="card-title">View our Annual report 2019</div>
+              <img class="arrow" src="<?php echo get_template_directory_uri() .
+                  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-12 col-md-4 mb-3">
+        <a href="/investors/event-calender/" class="card-link">
+          <div class="card">
+            <div class="card-header">
+              <img src="<?php echo get_template_directory_uri() .
+                  "/assets/images/investors/events-calendar.png"; ?>" alt="Events Calendar" loading="lazy">
+            </div>
+            <div class="card-body">
+              <div class="card-title">Events Calendar</div>
+              <img class="arrow" src="<?php echo get_template_directory_uri() .
+                  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-12 col-md-4 mb-3">
+        <a href="/about-us/overview/1537-2/" class="card-link">
+          <div class="card">
+            <div class="card-header">
+              <img src="<?php echo get_field(
+                  "why_invest_in_dana_gas"
+              ); ?>" alt="Why invest in Dana Gas" loading="lazy">
+            </div>
+            <div class="card-body">
+              <div class="card-title">Why invest in Dana Gas</div>
+              <img class="arrow" src="<?php echo get_template_directory_uri() .
+                  "/assets/images/right-arrow-green.png"; ?>" alt="Right arrow icon">
+            </div>
+          </div>
+        </a>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+<?php include get_theme_file_path("/parts/why-invest.php"); ?>
+
+<?php get_footer(); ?>
+<script src="<?php echo get_template_directory_uri() . "/dist/js/sustainability.min.js"; ?>"></script>
+
+</body>
+
+</html>
